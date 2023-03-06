2ff24766c119baa3530d524e3812bab359d8ae25
dei09 <dei09@hotmail.com>
2023-03-06 12:56:27 -0600

    Add project files.

---
+++ ProyectoFinalAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,24 @@
+﻿using ProyectoFinalAPI.Entities;
+using ProyectoFinalAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProyectoFinalAPI.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        UsuarioModel model = new UsuarioModel();
+
+        [HttpPost]
+        [Route("api/ValidarUsuario")]
+        public bool ValidarUsuario(UsuariosEnt entidad)
+        {
+            return model.ValidarUsuario(entidad);
+        }
+    }
+}
+
---
+++ ProyectoFinalAPI/Entities/UsuariosEnt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalAPI.Entities
+{
+    public class UsuariosEnt
+    {
+        public string correo { get; set; }
+        public string contrasenna { get; set; }
+
+    }
+}
