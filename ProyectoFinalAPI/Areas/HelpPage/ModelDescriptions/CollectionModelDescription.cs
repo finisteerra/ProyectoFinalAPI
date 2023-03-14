8fb81a5a6a5de33cf9f4621d47aa75aaa40aa2dd
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-13 19:53:02 -0600

    Delete ProyectoFinalAPI directory

--- ProyectoFinalAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ProyectoFinalAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- ProyectoFinalAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ProyectoFinalAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
