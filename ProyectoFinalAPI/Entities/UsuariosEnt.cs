ea013d7fedbfcd060780383e129e4e2ca680f884
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-13 22:14:33 -0600

    Modificacion de la BD, Entities y Controllers
    Modificacion de la BD, Entities y Controllers

---
+++ ProyectoFinalAPI/Entities/UsuariosEnt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalAPI.Models
+{
+    public class UsuariosEnt
+    {
+        public string CorreoElectronico { get; set; }
+        public string Contrasenna { get; set; }
+        public string ConfirmarContrasenna { get; set; }
+    }
+}
---
+++ ProyectoFinalAPI/Models/UsuarioModel.cs
@@ -0,0 +1,26 @@
+﻿using ProyectoFinalAPI.Entities;
+using ProyectoFinalAPI.ModeloBD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalAPI.Models
+{
+    public class UsuarioModel
+    {
+
+        public bool ValidarUsuario(UsuariosEnt entidad)
+        {
+            using (var conexion = new ProyectoProgramacionEntities())
+            {
+                var respuesta = conexion.ValidarUsuario(entidad.correo, entidad.contrasenna).FirstOrDefault();
+                if (respuesta == null)
+                    return false;
+
+                return true;
+            }
+        }
+
+        }
+    }
