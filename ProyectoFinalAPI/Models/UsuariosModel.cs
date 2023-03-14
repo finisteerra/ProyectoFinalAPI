dda32e944e4988f2ab7f7f53c0ceec13b51cb214
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-14 13:23:33 -0600

    Reconfiguracion de la BD y solucion de las views

---
+++ ProyectoFinalAPI/Models/UsuariosModel.cs
@@ -0,0 +1,24 @@
+﻿using ProyectoFinalAPI.Entities;
+using ProyectoFinalAPI.ModeloBD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalAPI.Models
+{
+    public class UsuariosModel
+    {
+        public bool ValidarUsuario(UsuariosEnt entidad)
+        {
+            using (var conexion = new ProyectoProgramacionEntities())
+            {
+                var respuesta = conexion.ValidarUsuario(entidad.CorreoElectronico, entidad.Contrasenna).FirstOrDefault();
+                if (respuesta == null)
+                    return false;
+
+                return true;
+            }
+        }
+    }
+}
