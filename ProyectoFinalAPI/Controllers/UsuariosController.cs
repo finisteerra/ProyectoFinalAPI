fb1db1c13a7d6997a7d0c73781c8c0d2bfac2dbc
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-14 13:20:12 -0600

    Actualizacion Entities, Controllers

---
+++ ProyectoFinalAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,21 @@
+﻿using ProyectoFinalAPI.Entities;
+using ProyectoFinalAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProyectoFinalAPI.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        UsuariosModel modelUsuarios = new UsuariosModel();
+
+        public bool ValidarUsuario(UsuariosEnt entidad)
+        {
+            return modelUsuarios.ValidarUsuario(entidad);
+        }
+    }
+}
