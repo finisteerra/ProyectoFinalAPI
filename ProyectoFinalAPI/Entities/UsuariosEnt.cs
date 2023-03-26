455dc9accedc2367caf07922d28c95c854c4de38
JAlejandroCC <98622443+JAlejandroCC@users.noreply.github.com>
2023-03-25 21:16:53 -0600

    Actualizar Usuarios ent

--- ProyectoFinalAPI/Entities/UsuariosEnt.cs
+++ ProyectoFinalAPI/Entities/UsuariosEnt.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +7,25 @@ namespace ProyectoFinalAPI.Entities
 {
     public class UsuariosEnt
     {
+        public long ConsecutivoUsuario { get; set; }
+
         public string CorreoElectronico { get; set; }
+
         public string Contrasenna { get; set; }
+
         public string ConfirmarContrasenna { get; set; }
+
+        public bool Estado { get; set; }
+
+        public string Token { get; set; }
+
+        public string Identificacion { get; set; }
+
+        public string Nombre { get; set; }
+
+        public byte IdProvincia { get; set; }
+
+        public long Rol { get; set; }
+
     }
 }
