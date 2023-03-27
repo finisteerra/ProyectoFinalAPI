775229eb42a55c03ed18c642e25b017c66f7b8f4
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-27 13:07:25 -0600

    Add files via upload

---
+++ ProyectoFinalAPI/Models/CevichesModels.cs
@@ -0,0 +1,38 @@
+﻿using ProyectoFinalAPI.Entities;
+using ProyectoFinalAPI.ModeloBD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalAPI.Models
+{
+    public class CevichesModels
+    {
+        public List<CevichesEnt> ConsultarCeviches()
+        {
+            using (var conexion = new ProyectoProgramacionEntities())
+            {
+                List<CevichesEnt> respuesta = new List<CevichesEnt>();
+                var datosBD = (from x in conexion.Ceviches
+                               select x).ToList();
+
+                if (datosBD.Count > 0)
+                {
+                    foreach (var item in datosBD)
+                    {
+                        respuesta.Add(new CevichesEnt
+                        {
+                            ConsecutivoCeviche = item.ConsecutivoCeviche,
+                            Descripcion = item.Descripcion,
+                            Cantidad = item.Cantidad,
+                            Precio = item.Precio
+                        });
+                    }
+                }
+
+                return respuesta;
+            }
+        }
+    }
+}
---
+++ ProyectoFinalAPI/Models/ProveedoresModels.cs
@@ -0,0 +1,39 @@
+﻿using ProyectoFinalAPI.Entities;
+using ProyectoFinalAPI.ModeloBD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalAPI.Models
+{
+    public class ProveedoresModels
+    {
+        public List<ProveedoresEnt> ConsultarProveedores()
+        {
+            using (var conexion = new ProyectoProgramacionEntities())
+            {
+                List<ProveedoresEnt> respuesta = new List<ProveedoresEnt>();
+                var datosBD = (from x in conexion.Proveedores
+                               select x).ToList();
+
+                if (datosBD.Count > 0)
+                {
+                    foreach (var item in datosBD)
+                    {
+                        respuesta.Add(new ProveedoresEnt
+                        {
+                            ConsecutivoProveedor = item.ConsecutivoProveedor,
+                            Nombre = item.Nombre,
+                            Apellido = item.Apellido,
+                            Compannia = item.Compañia,
+                            Pais = item.Pais
+                        });
+                    }
+                }
+
+                return respuesta;
+            }
+        }
+    }
+}
