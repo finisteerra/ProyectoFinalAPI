706a5bf2ac044b54848754ac371e5d6c65143835
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-27 13:01:09 -0600

    Token

---
+++ ProyectoFinalAPI/Controllers/CevichesController.cs
@@ -0,0 +1,22 @@
+﻿using ProyectoFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    public class CevichesController : Controller
+    {
+        CevichesModels modelCeviches = new CevichesModels();
+
+        // GET: Ceviches
+        [HttpGet]
+        public ActionResult ConsultarCeviches()
+        {
+            var resultado = modelCeviches.ConsultarCeviches();
+            return View(resultado);
+        }
+    }
+}
---
+++ ProyectoFinalAPI/Controllers/ProveedoresController.cs
@@ -0,0 +1,23 @@
+﻿using ProyectoFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        // GET: Provedores
+
+        ProveedoresModels modelProveedores = new ProveedoresModels();
+
+        [HttpGet]
+        public ActionResult ConsultarProveedores()
+        {
+            var resultado = modelProveedores.ConsultarProveedores();
+            return View(resultado);
+        }
+    }
+}
