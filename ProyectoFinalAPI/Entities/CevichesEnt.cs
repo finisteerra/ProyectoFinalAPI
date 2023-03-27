57256f7d9b1bae4568999ba12a4130049fb3dfdb
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-27 13:04:47 -0600

    Nuevas Entidades

---
+++ ProyectoFinalAPI/Entities/CevichesEnt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalAPI.Entities
+{
+    public class CevichesEnt
+    {
+        public long ConsecutivoCeviche { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int Cantidad { get; set; }
+    }
+}
---
+++ ProyectoFinalAPI/Entities/ProveedoresEnt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalAPI.Entities
+{
+    public class ProveedoresEnt
+    {
+        public long ConsecutivoProveedor { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Compannia { get; set; }
+
+        public string Pais { get; set; }
+    }
+}
