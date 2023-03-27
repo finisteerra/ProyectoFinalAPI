ec3380fb708eed7993abc444db96543ab6e53aab
MarioSanchez09 <114043756+MarioSanchez09@users.noreply.github.com>
2023-03-27 13:03:40 -0600

    Token

---
+++ ProyectoFinalAPI/Controllers/CevichesController.cs
@@ -0,0 +1,24 @@
+﻿using ProyectoFinalAPI.Entities;
+using ProyectoFinalAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProyectoFinalAPI.Controllers
+{
+    public class CevichesController : ApiController
+    {
+        CevichesModels modelCeviches = new CevichesModels();
+
+        [HttpGet]
+        //[Authorize]
+        [Route("api/ConsultarCeviches")]
+        public List<CevichesEnt> ConsultarCeviches()
+        {
+            return modelCeviches.ConsultarCeviches();
+        }
+    }
+}
---
+++ ProyectoFinalAPI/Controllers/ProveedoresController.cs
@@ -0,0 +1,24 @@
+﻿using ProyectoFinalAPI.Entities;
+using ProyectoFinalAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProyectoFinalAPI.Controllers
+{
+    public class ProveedoresController : ApiController
+    {
+        ProveedoresModels modelProveedores = new ProveedoresModels();
+
+        [HttpGet]
+        //[Authorize]
+        [Route("api/ConsultarProveedores")]
+        public List<ProveedoresEnt> ConsultarProveedores()
+        {
+            return modelProveedores.ConsultarProveedores();
+        }
+    }
+}
